4f25acc896dfcaa151579cce7cb310e141bd7e0f
Corvin Tiperciuc <cuvidk@gmail.com>
2021-04-12 17:26:32 +0300

    Commit 0

---
+++ src/ethereum/contracts/crowdfund.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.3;
+
+contract CrowdfundCampaign {
+  struct SpendingRequest {
+    string description;
+    uint value;
+    address payable recipient;
+    mapping(address => bool) approvals;
+    uint approvalsCount;
+    bool completed;
+  }
+
+  modifier ownerOnly {
+    require(msg.sender == owner, "Operation reserved for owner only");
+    _;
+  }
+
+  address public owner;
+  uint public minimumContribution;
+  mapping(address => bool) public contributors;
+  uint public contributorsCount;
+  SpendingRequest[] public requests;
+
+  constructor(uint minimum) {
+    minimumContribution = minimum;
+    owner = msg.sender;
+  }
+
+  function contribute() public payable {
+    require(msg.value >= minimumContribution, "Contribution amount too low");
+    require(!contributors[msg.sender], "You already funded this campaign");
+    contributors[msg.sender] = true;
+    contributorsCount++;
+  }
+
+  function createRequest(string memory description, uint value, address payable recipient) public ownerOnly {
+    SpendingRequest storage request = requests.push();
+    request.description = description;
+    request.value = value;
+    request.recipient = recipient;
+    request.approvalsCount = 0;
+    request.completed = false;
+  }
+
+  function approveRequest(uint id) public {
+    SpendingRequest storage request = requests[id];
+    require(contributors[msg.sender], "You need to contribute to the campaign before you can vote");
+    require(!request.approvals[msg.sender], "You already approved this request");
+    request.approvals[msg.sender] = true;
+    request.approvalsCount++;
+  }
+
+  function finalizeRequest(uint id) public payable ownerOnly {
+    SpendingRequest storage request = requests[id];
+    require(request.approvalsCount > contributorsCount / 2, "Not enough approvals");
+    require(!request.completed, "Request already completed");
+    request.recipient.transfer(request.value);
+    request.completed = true;
+  }
+}
