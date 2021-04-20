8306c78438f4242476aca3ffce253592aab1d4fb
Corvin Tiperciuc <cuvidk@gmail.com>
2021-04-20 17:36:36 +0300

    Rewrite New component using react hooks

--- src/pages/campaigns/new.js
+++ src/pages/campaigns/new.js
@@ -1,75 +1,65 @@
-import React from "react";
-import Layout from "../../components/Layout";
+import React, { useState } from "react";
+import { useRouter } from "next/router";
 import { Form, Button, Input, Message } from "semantic-ui-react";
+
+import Layout from "../../components/Layout";
 import factory from "../../CampaignFactory";
 import web3 from "../../web3";
 
-class New extends React.Component {
-  state = {
-    minimumContribution: "",
-    loading: false,
-    error: "",
-    success: "",
-  };
+export default function New() {
+  const [minContribution, setMinContribution] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+  const router = useRouter();
 
-  onSubmit = async (event) => {
+  async function onSubmit(event) {
     event.preventDefault();
-    this.setState({ loading: true, success: "", error: "" });
+    setLoading(true);
+    setError("");
+    setSuccess("");
 
     try {
       const accounts = await web3.eth.requestAccounts();
 
       await factory.methods
-        .createCampaign(this.state.minimumContribution)
+        .createCampaign(minContribution)
         .send({
           from: accounts[0],
         })
         .once("confirmation", () => {
-          this.setState({
-            loading: false,
-            success: "Campaign created with success !",
-          });
+          setLoading(false);
+          setSuccess("Campaign created with success !");
+          router.push("/");
         });
     } catch (err) {
       console.log(err);
-      this.setState({ loading: false, error: err.message });
+      setLoading(false);
+      setError(err.message);
     }
-  };
-
-  render() {
-    return (
-      <Layout>
-        <h3>Create a Campaign</h3>
-        <Form
-          onSubmit={this.onSubmit}
-          error={!!this.state.error}
-          success={!!this.state.success}
-        >
-          <Form.Field>
-            <label>Minimum Contribution</label>
-            <Input
-              label="wei"
-              labelPosition="right"
-              value={this.state.minimumContribution}
-              onChange={(event) => {
-                this.setState({ minimumContribution: event.target.value });
-              }}
-            ></Input>
-          </Form.Field>
-          <Message success header="Yey!" content={this.state.success} />
-          <Message error header="Oups!" content={this.state.error} />
-          <Button
-            type="submit"
-            loading={this.state.loading}
-            disabled={this.state.loading}
-            primary
-          >
-            Create
-          </Button>
-        </Form>
-      </Layout>
-    );
   }
-}
 
-export default New;
+  return (
+    <Layout>
+      <h3>Create a Campaign</h3>
+      <Form onSubmit={onSubmit} error={!!error} success={!!success}>
+        <Form.Field>
+          <label>Minimum Contribution</label>
+          <Input
+            label="wei"
+            labelPosition="right"
+            value={minContribution}
+            onChange={(event) => {
+              setMinContribution(event.target.value);
+            }}
+          ></Input>
+        </Form.Field>
+        <Message success header="Yey!" content={success} />
+        <Message error header="Oups!" content={error} />
+        <Button type="submit" loading={loading} disabled={loading} primary>
+          Create
+        </Button>
+      </Form>
+    </Layout>
+  );
+}
