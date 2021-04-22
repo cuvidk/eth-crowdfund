a594396483cdeeb33c9f5c6f904774e037a05fd1
Corvin Tiperciuc <cuvidk@gmail.com>
2021-04-22 15:53:24 +0300

    Converts Index into a functional component Added links to various page elements Redirects to home page after succesful campaign creation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,20 @@
 import React from "react";
 import { Menu } from "semantic-ui-react";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <Menu style={{ marginTop: "10px" }}>
-      <Menu.Item>Crowdfund</Menu.Item>
+      <Link href="/" passHref>
+        <Menu.Item>Crowdfund</Menu.Item>
+      </Link>
       <Menu.Menu position="right">
-        <Menu.Item>Campaigns</Menu.Item>
-        <Menu.Item>+</Menu.Item>
+        <Link href="/" passHref>
+          <Menu.Item>Campaigns</Menu.Item>
+        </Link>
+        <Link href="/campaigns/new" passHref>
+          <Menu.Item>+</Menu.Item>
+        </Link>
       </Menu.Menu>
     </Menu>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,38 +1,48 @@
 import React from "react";
 import { Button, Container, Card } from "semantic-ui-react";
+import Link from "next/link";
+
 import web3 from "../web3";
 import factory from "../CampaignFactory";
 import "../components/Layout";
 import Layout from "../components/Layout";
 
-export default class Index extends React.Component {
-  static async getInitialProps(context) {
-    const campaigns = await factory.methods.getCampaigns().call();
-    return { campaigns };
-  }
+const Index = ({ campaigns }) => {
+  const items = campaigns.map((campaign) => {
+    return {
+      header: campaign,
+      description: (
+        <Link href={`/campaigns/${campaign}`} passHref>
+          View campaign
+        </Link>
+      ),
+      fluid: true,
+    };
+  });
 
-  renderCampaigns() {
-    return this.props.campaigns.map((campaign) => {
-      return {
-        header: campaign,
-        description: <a>View campaign</a>,
-        fluid: true,
-      };
-    });
-  }
-
-  render() {
-    return (
-      <Layout>
-        <h3>Campaigns</h3>
+  return (
+    <Layout>
+      <h3>Campaigns</h3>
+      <Link href="/campaigns/new" passHref>
         <Button
           content="Create Campaign"
           icon="plus circle"
           floated="right"
           primary
         />
-        <Card.Group items={this.renderCampaigns()} />
-      </Layout>
-    );
-  }
+      </Link>
+      <Card.Group items={items} />
+    </Layout>
+  );
+};
+
+export async function getServerSideProps() {
+  const campaigns = await factory.methods.getCampaigns().call();
+  return {
+    props: {
+      campaigns,
+    },
+  };
 }
+
+export default Index;
